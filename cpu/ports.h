18ff93c0f3ee6aa3d9aa394f7da4901bf0496168
Bogdan Rogoz <bogdanius96@gmail.com>
2024-05-21 23:39:25 +0300

    added base os

---
+++ cpu/ports.h
@@ -0,0 +1,13 @@
+#ifndef PORTS_H
+#define PORTS_H
+
+#include <stdint.h>
+
+unsigned char port_byte_in (uint16_t port);
+void port_byte_out (uint16_t port, uint8_t data);
+unsigned short port_word_in (uint16_t port);
+void port_word_out (uint16_t port, uint16_t data);
+unsigned short port_words_in (uint16_t port, void *buffer, uint16_t length);
+void port_words_out (uint16_t port, void *buffer, uint16_t length);
+
+#endif
---
+++ libc/globals.h
@@ -0,0 +1,5 @@
+#ifndef _OS_GLOBALS_H
+#define _OS_GLOBALS_H
+extern uint32_t TestIntNumber;
+
+#endif
